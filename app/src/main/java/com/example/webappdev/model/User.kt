30a1026681bd551636e88fe92d5c987a75db8e1d
KiseraTimon <timonmarick@gmail.com>
2025-11-16 22:38:49 +0300

    Updated User Model to Work with Firebase

--- app/src/main/java/com/example/webappdev/model/User.kt
+++ app/src/main/java/com/example/webappdev/model/User.kt
@@ -2,7 +2,7 @@ package com.example.webappdev.model
 
 // Logged-in User Data
 data class User(
-    val name: String,
-    val email: String,
+    val name: String = "",
+    val email: String = "",
     var points: Int = 0
 )
